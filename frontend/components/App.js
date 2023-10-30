a26f3d365e4060e85d3311119bdc1ecd3aaab89e
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-30 11:02:09 -0700

    Logout should say goodbye, remove the current token, and redirect to login

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -31,6 +31,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    setMessage("Goodbye!")
+    localStorage.removeItem("token")
+    redirectToLogin()
   }
 
   const login = ({ username, password }) => {
