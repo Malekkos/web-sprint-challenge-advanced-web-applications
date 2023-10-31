65874017d85a8b49a00221b983338f0f98e48edb
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-31 07:34:49 -0700

    Render spinner works, sanity check works as intended kind of

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,13 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import Spinner from "./Spinner"
+import { render } from "@testing-library/react"
+import React from "react"
+
+
 test('sanity', () => {
   expect(true).toBe(false)
+})
+test("render spinner works", () => {
+  render(<Spinner />)
 })
