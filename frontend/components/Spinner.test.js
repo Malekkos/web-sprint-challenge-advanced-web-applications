17db0435982ebf8a6167abd60d5c13630096e7f6
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-02 08:55:55 -0700

    Finished the test. I think its the only test due to Spinner recieving only one prop, 'on', which is either true or false

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,7 +1,7 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 import Spinner from "./Spinner"
-import { render, screen, queryByAttribute } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import React from "react"
 
 test('sanity', () => {
@@ -11,10 +11,10 @@ test("render spinner works wihtout errors", () => {
   render(<Spinner />)
 })
 test("render spinner with a passed prop of 'on' as 'true', should display said spinner as well as the text 'Please wait...' ", () => {
-  render(<Spinner on={true} />)
-  const spinnerMessage = screen.getByText(/please wait.../i)
-  const spinner = document.getElementById("spinner")
-  // console.log(spinnerMessage)
-  // console.log(spinner)
-  expect(spinnerMessage).toHaveTextContent("Please wait...")
+  const {rerender} = render(<Spinner on={true} />)
+  const text = screen.queryByText("Please wait...")
+  console.log(text.id)
+  expect(text).toBeTruthy()
+  rerender(<Spinner on={false} />)
+  expect(screen.queryByText("Please wait...")).toBeFalsy()
 })
